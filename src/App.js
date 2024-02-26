74bcad69274968a236a647e6fc808614d21e078c
itmevasudevan <90838917+itmevasudevan@users.noreply.github.com>
2024-02-26 23:45:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const MyComponent = ({ title, backgroundColor, children }) => {
   return (
     <div className="col-12" >
     <div className="component" style={componentStyle}>
-      <h2>{title}</h2>
+      <h5>{title}</h5>
       <div className="resizable" style={{ width: '100%', height: '100%' }} onChange={() => setComponentStyle({ ...componentStyle })}>
         {children}
       </div>
@@ -39,17 +39,27 @@ const MyComponent = ({ title, backgroundColor, children }) => {
  * @return {JSX.Element} The rendered main App component
  */
 const App = () => {
-  const [formData, setFormData] = useState({ fname: '', lname: '', mobile: '' }); // State variable to track form data
-  const [formSubmitted, setFormSubmitted] = useState(false); // State variable to track form submission
-  const [tableData, setTableData] = useState([]); // State variable to track table data
+  const [formData, setFormData] = useState({ fname: '', lname: '', mobile: '' }); 
+  const [formSubmitted, setFormSubmitted] = useState(false); 
+  const [tableData, setTableData] = useState([]); 
   const [editIndex, setEditIndex] = useState(null);
-
-
+  const [addDataCount, setAddDataCount] = useState(0);
+  const [updateDataCount, setUpdateDataCount] = useState(0);
+  console.log(formSubmitted);
   // Fetch data on component mount
   useEffect(() => {
     fetchData();
+    updateDashboard();
   }, []);
 
+
+  const updateDashboard = async() => {
+    const response = await axios.get('https://work-backend-api-dev.azurewebsites.net/get_count');
+    console.log(response.data.addCount)
+    setAddDataCount(response.data.addCount);
+    setUpdateDataCount(response.data.updateCount);
+ 
+  };
 
     /**
    * Asynchronous function to fetch data from 'API' using axios, 
@@ -59,9 +69,9 @@ const App = () => {
    * @return {Promise<void>} 
    */
   const fetchData = async () => {
-    setTableData([{id:1,fname:"sam",lname:"kumar",mobile:"1234567890"},{id:2,fname:"jhon",lname:"m",mobile:"343434"}]);
+    
     try {
-      const response = await axios.get('http://localhost:8083/get_users');
+      const response = await axios.get('https://work-backend-api-dev.azurewebsites.net/get_users');
       setTableData(response.data); // Assuming the API response is an array of objects
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -78,28 +88,26 @@ const App = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      // Send the form data to the server
-      const formData = {
+       const formData = {
         fname: event.target.fname.value,
         lname: event.target.lname.value,
         mobile: event.target.mobile.value
       };
 
       if (editIndex !== null) {
-        // If editing, call the update API
-        await axios.put(`http://localhost:8083/add_data/${editIndex}`, formData);
-        // Update the data in the tableData state
+        await axios.put(`https://work-backend-api-dev.azurewebsites.net/add_data/${editIndex}`, formData);
         setFormSubmitted(true);
         fetchData();
-        setEditIndex(null); // Reset edit index after update
-      } else {
-        const response = await axios.post('http://localhost:8083/add_data', formData);
+        setEditIndex(null); 
+      } else {      
+        const response = await axios.post('https://work-backend-api-dev.azurewebsites.net/add_data', formData);
         console.log('Form submitted successfully:', response.data);
 
-        // Optionally, you can reset the form after successful submission
         setFormData({ fname: '', lname: '', mobile: '' });
-        setFormSubmitted(true);
+        setFormSubmitted(true);      
         fetchData();
+        updateDashboard();
+        alert('Form submitted successfully');
       }
     } catch (error) {
       console.error('Error submitting form:', error);
@@ -120,13 +128,11 @@ const App = () => {
 
   const handleEdit = (id) => {
     console.log("edit", id);
-    // Find the index of the item with the given id in the tableData array
-    const index = tableData.findIndex(item => item.id === id);
-    if (index !== -1) {
-      // If the item is found, set form data to the selected row's data for editing
+       const index = tableData.findIndex(item => item.user_uid === id);
+    if (index !== -1) {    
       const selectedData = tableData[index];
       setFormData(selectedData);
-      setEditIndex(index);
+      setEditIndex(id);
     } else {
       console.error("Item with id", id, "not found.");
     }
@@ -135,30 +141,44 @@ const App = () => {
 
   // Define the layout of the grid
   const layout = [
-    { i: '1', x: 0, y: 0, w: 4, h: 10 },
-    { i: '2', x: 4, y: 0, w: 4, h: 6 },
-    { i: '3', x: 8, y: 0, w: 4, h: 6 },
+    { i: '1', x: 0, y: 0, w: 4, h: 4 },
+    { i: '2', x: 4, y: 0, w: 4, h: 4 },
+    { i: '3', x: 8, y: 0, w: 4, h: 4 },
   ];
 
   return (
     // Main App component
     <div className="container-fluid">
       <div className="row">
-        
-  
         <div className="app">
-          {/* Grid layout component with specified layout, cols, and rowHeight */}
-          <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-            {/* Three MyComponent instances with specified keys,titles,background colors, and children */}
+           <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
             <div key="1">
-              {/* MyComponent component with specified title, background color, and children */}
-              <MyComponent title="Component 1" backgroundColor="#ffcccc" onResizeStop={() => console.log('Resize stopped')}>
+               <MyComponent title="Component 1" backgroundColor="#ffcccc" onResizeStop={() => console.log('Resize stopped')}>
+              <h4>Total Add Count</h4>
+              <h2>{addDataCount}</h2>
+              
+              </MyComponent>
+            </div>
+            <div key="2">
+             <MyComponent title="Component 2" backgroundColor="#ccffcc">
+              
+              <h4>Total Update Count</h4>
+              <h2>{updateDataCount}</h2>
 
-                {formSubmitted && (
-                  <div className="alert alert-success" role="alert">
-                    Form submitted successfully!
-                  </div>
-                )}
+              </MyComponent>
+            </div>
+            <div key="3">
+              <MyComponent title="Component 3" backgroundColor="#ccccff">
+                <p>This is a paragraph inside Component 3,This is a paragraph inside Component 3,This is a paragraph inside Component 3.</p>
+              </MyComponent>
+            </div>
+          </GridLayout>
+        </div>
+
+      </div>
+
+      <div className="row mt-5">
+        <div className="col-6">
 
                 <form onSubmit={handleSubmit}>
                   <div className="mb-3">
@@ -173,15 +193,14 @@ const App = () => {
                     <label htmlFor="mobile" className="form-label">Mobile:</label>
                     <input type="tel" className="form-control" id="mobile" name="mobile" value={formData.mobile} onChange={handleInputChange} />
                   </div>
-                  <button type="submit" className="btn btn-primary">Submit</button>
+                  <button type="submit" className="btn btn-primary">{editIndex!=null?'Update':'Submit'}</button>
                 </form>
-              </MyComponent>
-            </div>
-            <div key="2">
-              {/* MyComponent component with specified title, background color, and children */}
-              <MyComponent title="Component 2" backgroundColor="#ccffcc">
-                {/* Table component with specified className */}
-                <table className="table table-striped">
+         
+        </div>
+
+        <div className="col-6">
+         
+          <table className="table table-striped">
                   <thead className="thead-dark">
                     <tr>
                       <th>First Name</th>
@@ -190,31 +209,20 @@ const App = () => {
                       <th>Action</th>
                     </tr>
                   </thead>
-                  <tbody>
-                    {/* Data to be dynamically populated */}
+                  <tbody>               
                     {tableData?.map((item, index) => (
                       <tr key={index}>
                         <td>{item.fname}</td>
                         <td>{item.lname}</td>
                         <td>{item.mobile}</td>
                         <td>
-                          <button type="button" className="btn btn-primary btn-sm" onMouseOver={() => handleEdit(item.id)}>Edit</button>
+                          <button type="button" className="btn btn-primary btn-sm" onClick={() => handleEdit(item.user_uid)}>Edit</button>
                         </td>
                       </tr>
                     ))}
                   </tbody>
-                </table>
-
-              </MyComponent>
-            </div>
-            <div key="3">
-              <MyComponent title="Component 3" backgroundColor="#ccccff">
-                <p>This is a paragraph inside Component 3.</p>
-              </MyComponent>
-            </div>
-          </GridLayout>
+                </table>         
         </div>
-
       </div>
     </div>
 
